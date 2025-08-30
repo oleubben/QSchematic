5bbd590f80cecf96e2d2fdb014557136cbe27d60
Ole Ubben <ole.ubben@gmail.com>
2025-08-30 18:46:35 +0200

    Make _scene proteced

--- qschematic/view.hpp
+++ qschematic/view.hpp
@@ -4,108 +4,95 @@
 
 #include <QGraphicsView>
 
-namespace QSchematic
-{
+namespace QSchematic {
+
+/**
+ * A QWidget to display & interact with a QSchematic scene.
+ */
+class View : public QGraphicsView {
+    Q_OBJECT
+    Q_DISABLE_COPY_MOVE(View)
+
+  public:
+    enum Mode { NormalMode, PanMode };
+
+    qreal zoom_factor_min = 0.25;
+    qreal zoom_factor_max = 10.0;
+    qreal zoom_factor_step = 0.05;
+    qreal fitall_padding = 20.0;
 
     /**
-     * A QWidget to display & interact with a QSchematic scene.
+     * Constructor.
+     *
+     * @param parent The parent widget.
      */
-    class View :
-        public QGraphicsView
-    {
-        Q_OBJECT
-        Q_DISABLE_COPY_MOVE(View)
+    explicit View(QWidget *parent = nullptr);
 
-    public:
-        enum Mode {
-            NormalMode,
-            PanMode
-        };
+    /**
+     * Destructor.
+     */
+    ~View() override = default;
 
-        qreal zoom_factor_min  = 0.25;
-        qreal zoom_factor_max  = 10.0;
-        qreal zoom_factor_step = 0.05;
-        qreal fitall_padding   = 20.0;
+    /**
+     * Set the scene.
+     *
+     * @param scene The scene.
+     */
+    void setScene(Scene *scene);
 
-        /**
-         * Constructor.
-         *
-         * @param parent The parent widget.
-         */
-        explicit
-        View(QWidget* parent = nullptr);
+    /**
+     * Set settings.
+     *
+     * @param settings The settings.
+     */
+    void setSettings(const Settings &settings);
 
-        /**
-         * Destructor.
-         */
-        ~View() override = default;
+    /**
+     * Get current zoom value.
+     *
+     * @return Current zoom value.
+     */
+    [[nodiscard]]
+    qreal zoomValue() const;
 
-        /**
-         * Set the scene.
-         *
-         * @param scene The scene.
-         */
-        void
-        setScene(Scene* scene);
+  Q_SIGNALS:
+    void zoomChanged(qreal factor);
+    void modeChanged(Mode newMode);
 
-        /**
-         * Set settings.
-         *
-         * @param settings The settings.
-         */
-        void
-        setSettings(const Settings& settings);
+  public Q_SLOTS:
+    /**
+     * Set the zoom value.
+     *
+     * @param factor The zoom value.
+     */
+    void setZoomValue(qreal factor);
 
-        /**
-         * Get current zoom value.
-         *
-         * @return Current zoom value.
-         */
-        [[nodiscard]]
-        qreal
-        zoomValue() const;
+    /**
+     * Fit everything into the view port.
+     *
+     * @details This modifies the view transformation to ensure that all items
+     * in the scene are visible in the viewport.
+     */
+    void fitInView();
 
-    Q_SIGNALS:
-        void zoomChanged(qreal factor);
-        void modeChanged(Mode newMode);
+  protected:
+    // QGraphicsView overrides
+    void keyPressEvent(QKeyEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
 
-    public Q_SLOTS:
-        /**
-         * Set the zoom value.
-         *
-         * @param factor The zoom value.
-         */
-        void
-        setZoomValue(qreal factor);
+    Scene *_scene = nullptr;
 
-        /**
-         * Fit everything into the view port.
-         *
-         * @details This modifies the view transformation to ensure that all items in the scene are visible in the
-         *          viewport.
-         */
-        void
-        fitInView();
+  private:
+    void updateScale();
 
-    protected:
-        // QGraphicsView overrides
-        void keyPressEvent(QKeyEvent* event) override;
-        void wheelEvent(QWheelEvent* event) override;
-        void mouseMoveEvent(QMouseEvent* event) override;
-        void mousePressEvent(QMouseEvent* event) override;
-        void mouseReleaseEvent(QMouseEvent* event) override;
+    void setMode(Mode newMode);
 
-    private:
-        void
-        updateScale();
-
-        void
-        setMode(Mode newMode);
-
-        Scene* _scene = nullptr;
-        Settings _settings;
-        qreal _scaleFactor = 1.0;
-        Mode _mode = Mode::NormalMode;
-        QPoint _panStart;
-    };
-}
+    Settings _settings;
+    qreal _scaleFactor = 1.0;
+    Mode _mode = Mode::NormalMode;
+    QPoint _panStart;
+};
+} // namespace QSchematic
